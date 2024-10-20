59995780db7cf2578c3e1793d334ff85975ef01d
Wassay Haider <74355781+malikwassay@users.noreply.github.com>
2024-10-20 18:42:48 +0500

    Add files via upload

--- src/DengueInfoPage.js
+++ src/DengueInfoPage.js
@@ -3,26 +3,41 @@ import './DengueInfoPage.css';
 
 const DengueInfoPage = () => {
   return (
-    <div className="dengue-info-page">
-      <header className="header">
+    <div className="container">
+      <header>
         <div className="logo">
-          <img src="/images/mosquito-icon.png" alt="Mosquito icon" />
+        <img src="/images/mosquito-icon.png" alt="Mosquito icon" />
         </div>
-        
-        <div className="author">
-          <p>Wassay Haider</p>
+        <div className="user-info">
+          <div className="user-avatar">
+          <img src="/images/user.png" alt="Mosquito icon" />
+          </div>
+          <span className="user-name">Wassay Haider</span>
         </div>
       </header>
-      <main className="content">
-      <h1>Dengue Epidemic</h1>
+      
+      <main>
+        <h1>Dengue Epidemic</h1>
+        <div className="underline"></div>
         <p className="description">a mosquito-borne tropical disease caused by dengue virus.</p>
-        <div className="button-container">
-          <button className="button visuals-button">Visuals →</button>
-          <button className="button graphs-button">Graphs →</button>
+        
+        <div className="illustration">
+        <img src="/images/dengue-illustration.png" alt="Dengue illustration" />
+        </div>
+        
+        <div className="buttons">
+          <button className="btn visuals">
+            Visuals
+            <span className="arrow">→</span>
+          </button>
+          <button className="btn graphs">
+            Graphs
+            <span className="arrow">→</span>
+          </button>
         </div>
       </main>
     </div>
   );
-};
+}
 
 export default DengueInfoPage;
